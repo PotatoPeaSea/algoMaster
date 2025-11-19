e1568cfd33446335b25cd7daa4f63a10e67a08f1
PotatoPeaSea <matthewmoxuanlin@gmail.com>
2025-11-19 14:24:41 -0800

    did 2 questions
    Leetcode 282: Move Zeroes
    Leetcode 169: Majority Element

---
+++ moveZeros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void moveZeroes(vector<int>& nums);
+int main() {
+
+}
+
+void moveZeroes(vector<int>& nums){
+    int zeros = 0;
+    for(int i = 0; i<nums.size(); i++){
+        if(nums[0])
+    }
+}
